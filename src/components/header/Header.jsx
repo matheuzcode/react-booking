e547146ad9c1eb9c5eb29763a0d90a37de890098
Matheus Monteiro <matheus_wellyson2015@hotmail.com>
2023-04-26 23:49:07 -0300

    filter list

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,8 +13,10 @@ import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {useState} from 'react';
 import {format} from 'date-fns';
+import { useNavigate } from "react-router-dom";
 
 const Header = ({type}) => {
+	const [destination, setDestination] = useState("");
 	const [openDate, setOpenDate] = useState(false);
 
 	const [date, setDate] = useState([
@@ -34,6 +36,8 @@ const Header = ({type}) => {
 
 	});
 
+	const navigate = useNavigate()
+
 	const handleOption = (name, operation) => {
 		setOptions((prev) => {
 			return {
@@ -41,6 +45,10 @@ const Header = ({type}) => {
 			};
 		});
 	};
+
+	const handleSearch = () => {
+		navigate("/hotels", { state: { destination, date, options } });
+	}
 
 	return (
 		<div className="header">
@@ -81,6 +89,7 @@ const Header = ({type}) => {
 							type="text"
 							placeholder="Where are you going?"
 							className="headerSearchInput"
+							onChange={(e) => setDestination(e.target.value)}
 							/>
 						</div>
 						<div className="headerSearchItem" >
@@ -91,6 +100,7 @@ const Header = ({type}) => {
 							   onChange={item => setDate([item.selection])}
 							   moveRangeOnFirstSelection={false}
 							   ranges={date}
+							   minDate={new Date()}
 							   className='date'
 							/>}
 						</div>
@@ -127,7 +137,7 @@ const Header = ({type}) => {
 							}
 						</div>
 						<div className="headerSearchItem">
-							<button className="headerBtn">Search</button>
+							<button className="headerBtn" onClick={handleSearch}>Search</button>
 						</div>
 					</div>
 					</>}
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,19 @@
 import './List.css'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
-
+import {useState} from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+import { useLocation } from 'react-router-dom';
+import {format} from 'date-fns';
+import { DateRange } from "react-date-range";
 const List = () => {
+
+	const location = useLocation();
+	const [destination, setDestination] = useState(location.state.destination)
+	const [date, setDate] = useState(location.state.date)
+	const [openDate, setOpenDate] = useState(false)
+	const [options, setOptions] = useState(location.state.options)
+
+	console.log(location)
 	return (
 		<div>
 			<Navbar/>
@@ -13,7 +24,17 @@ const List = () => {
 						<h1 className="lsTitle">Search</h1>
 						<div className="lsItem">
 							<label>Destination</label>
-							<input type="text"/>
+							<input placeholder={destination} type="text"/>
+						</div>
+						<div className="lsItem">
+							<label>Check-in Date</label>
+							<span onClick={()=>setOpenDate(!openDate)} >{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+							{ openDate && (
+							<DateRange
+							onChange={item=>setDate([item.selection])}
+							minDate={new Date()}
+							ranges={date}
+							/>)}
 						</div>
 					</div>
 					<div className="listResult"></div>
