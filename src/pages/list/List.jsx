cbc0a4caf6037493454627da66d0611dd9393375
Matheus Monteiro <matheus_wellyson2015@hotmail.com>
2023-04-29 23:49:13 -0300

    serach item

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -2,6 +2,7 @@ import './List.css'
 import {useState} from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import SearchItem from '../../components/searchItem/SearchItem';
 import { useLocation } from 'react-router-dom';
 import {format} from 'date-fns';
 import { DateRange } from "react-date-range";
@@ -36,8 +37,53 @@ const List = () => {
 							ranges={date}
 							/>)}
 						</div>
+						<div className="lsItem">
+							<label>Options</label>
+							<div className="lsOptions">
+								<div className="lsOptionItem">
+									<span className="lsOptionText">
+										Min price <small>per night</small>
+									</span>
+									<input type="number" className="lsOptionInput"/>
+								</div>
+								<div className="lsOptionItem">
+									<span className="lsOptionText">
+										Max price <small>per night</small>
+									</span>
+									<input type="number" className="lsOptionInput"/>
+								</div>
+								<div className="lsOptionItem">
+									<span className="lsOptionText">
+										Adult
+									</span>
+									<input type="number" min={1} className="lsOptionInput" placeholder={options.adult}/>
+								</div>
+								<div className="lsOptionItem">
+									<span className="lsOptionText">
+										Children
+									</span>
+									<input type="number" min={0} className="lsOptionInput" placeholder={options.children}/>
+								</div>
+								<div className="lsOptionItem">
+									<span className="lsOptionText">
+										Room
+									</span>
+									<input type="number" min={1} className="lsOptionInput" placeholder={options.room}/>
+								</div>
+							</div>
+						</div>
+						<button>Search</button> 
 					</div>
-					<div className="listResult"></div>
+					<div className="listResult">
+						<SearchItem/>
+						<SearchItem/>
+						<SearchItem/>
+						<SearchItem/>
+						<SearchItem/>
+						<SearchItem/>
+						<SearchItem/>
+						<SearchItem/>
+					</div>
 				</div>
 			</div>
 		</div>
